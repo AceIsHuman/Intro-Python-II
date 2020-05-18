2603931e0fef9c35f5e30bd6c7022b3ad662d162
AceIsHuman <menoman1995@yahoo.com>
2020-05-17 23:45:19 -0500

    show items in room

--- src/room.py
+++ src/room.py
@@ -11,4 +11,8 @@ class Room:
     wrap = textwrap.TextWrapper(width = 40)
     output = f'{self.name}:\n'
     output += wrap.fill(text = '    ' + self.description)
+    if len(self.items):
+      output += '\n\nThere is something on the ground:\n'
+      for item in self.items:
+        output += f'    - {item}'
     return output
