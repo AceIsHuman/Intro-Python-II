bc03de7e96eb7f2e3b1eedc37177f79a8b338883
AceIsHuman <menoman1995@yahoo.com>
2020-05-20 21:20:17 -0500

    define on_drop

--- src/item.py
+++ src/item.py
@@ -1,10 +1,13 @@
 class Item:
-  def __init__(self, name, description):
-    self.name = name
-    self.description = description
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def on_take(self):
-    print(f'You have picked up {self.name}')
+    def on_take(self):
+        print(f'You have picked up {self.name}.')
+
+    def on_drop(self):
+        print(f'You have dropped {self.name}.')
