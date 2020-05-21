6499923b184e56053f68ba9c2e41385465fe081e
AceIsHuman <menoman1995@yahoo.com>
2020-05-20 21:51:26 -0500

    update __str__

--- src/item.py
+++ src/item.py
@@ -4,7 +4,7 @@ class Item:
         self.description = description
 
     def __str__(self):
-        return self.name
+        return f'{self.name} - {self.description}'
 
     def on_take(self):
         print(f'You have picked up {self.name}.')
